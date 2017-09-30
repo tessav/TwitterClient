d7e9ebf09c2b3cb6c3c9a6cfd6ac6bb8930a2cfe
tessav <tessavoon@gmail.com>
2017-09-29 19:09:42 -0700

    char counting

--- app/src/main/java/com/codepath/apps/restclienttemplate/activities/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/activities/ComposeActivity.java
@@ -2,13 +2,18 @@ package com.codepath.apps.restclienttemplate.activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.codepath.apps.restclienttemplate.R;
@@ -24,6 +29,7 @@ public class ComposeActivity extends AppCompatActivity {
     @BindView(R.id.ivProfileImage) ImageView ivProfileImage;
     @BindView(R.id.btnTweet) Button btnTweet;
     @BindView(R.id.etTweetBody) EditText etTweetBody;
+    @BindView(R.id.tvMsgCount) TextView tvMsgCount;
     @BindView(R.id.ivCancel) ImageView ivCancel;
 
     private TwitterClient client;
@@ -37,6 +43,7 @@ public class ComposeActivity extends AppCompatActivity {
         context = getApplicationContext();
         client = TwitterApp.getRestClient();
         setupToolbar();
+        setupMessageListener();
         setupSubmitListener();
         setupCancelListener();
         Glide.with(context)
@@ -51,6 +58,28 @@ public class ComposeActivity extends AppCompatActivity {
         getSupportActionBar().setElevation(10);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().setDisplayShowCustomEnabled(true);
+    }
+
+    private void setupMessageListener() {
+        disableButton();
+        etTweetBody.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Log.d("count", String.valueOf(count));
+                int charLeft = 140 - (start + count);
+                changeCountFeedback(charLeft);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
     private void setupSubmitListener() {
@@ -80,6 +109,33 @@ public class ComposeActivity extends AppCompatActivity {
 
     private void closeForm() {
         this.finish();
+    }
+
+    private void changeCountFeedback(int charLeft) {
+        tvMsgCount.setText(String.valueOf(charLeft));
+        if (charLeft == 140) {
+            disableButton();
+        } else if (charLeft < 0) {
+            tvMsgCount.setTextColor(Color.RED);
+            disableButton();
+        } else {
+            tvMsgCount.setTextColor(Color.GRAY);
+            enableButton();
+        }
+    }
+
+    private void disableButton() {
+        if (btnTweet.isEnabled()) {
+            btnTweet.setBackgroundColor(getResources().getColor(R.color.disabledBtn));
+            btnTweet.setEnabled(false);
+        }
+    }
+
+    private void enableButton() {
+        if (!btnTweet.isEnabled()) {
+            btnTweet.setBackgroundColor(getResources().getColor(R.color.twitter));
+            btnTweet.setEnabled(true);
+        }
     }
 
 
